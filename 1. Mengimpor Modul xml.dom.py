dcd64ba169657a3baab8638595a46ec0d5c3dd7f
rendhive <rendhivecode@gmail.com>
2026-06-14 22:52:00 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul xml.dom.py

---
+++ 1. Mengimpor Modul xml.dom.py
@@ -0,0 +1,5 @@
+from xml.dom import minidom
+
+print("Modul xml.dom berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan DOM API dengan minidom.
+# Kondisi: Digunakan sebelum memanipulasi atau mem-parsing file XML.
